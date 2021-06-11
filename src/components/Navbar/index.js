5aee830c31423111fff69e5fd1428361b8af3fea
Sarthak <sarthaksingh0608@gmail.com>
2021-06-11 22:51:00 +0530

    added Navbar and home page

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav,NavbarContainer,NavLogo,MobileIcon,NavMenu,NavItem,NavLinks,NavBtn,NavBtnLink} from "./NavbarElement";
+
+const Navbar = ({toggle}) => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>NoTaTo</NavLogo>
+                   <MobileIcon onClick={toggle}>
+                       <FaBars/>
+                   </MobileIcon>
+                   <NavMenu>
+                       <NavItem>
+                           <NavLinks to ='about'>About</NavLinks>
+                       </NavItem>
+                       <NavItem>
+                           <NavLinks to ='discover'>Discover</NavLinks>
+                       </NavItem>
+                       <NavItem>
+                           <NavLinks to ='services'>Services</NavLinks>
+                       </NavItem>
+                       <NavItem>
+                           <NavLinks to ='signup'>Sign-Up</NavLinks>
+                       </NavItem>
+                       <NavBtn>
+                           <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                       </NavBtn>
+                   </NavMenu>
+                
+             </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
