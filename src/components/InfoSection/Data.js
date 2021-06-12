689f75c67696890421a4ea460ce1d462e1e76254
Sarthak <sarthaksingh0608@gmail.com>
2021-06-12 23:59:08 +0530

    created info section and added css property

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Exclusive Notes',
+    headline:'Notes Of All Chapter Of Each Subject Of Every Semester',
+    description:'Get access to our exclusive app that allows you to access notes and also with some special feature',
+    buttonLabel:'Get Started',
+    imgStart:false,
+    img:require('../../images/svg-1.svg'),
+    alt:'pic',
+    dark:true,
+    primary:true,
+    darkText:false
+}
